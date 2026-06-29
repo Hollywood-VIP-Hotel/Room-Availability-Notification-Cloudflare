85d0f6a2a1a0ee12f32d628c32542696bc24216f
Hollywood-VIP-Hotel <viphotel28@gmail.com>
2026-06-29 16:45:20 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,10 +19,19 @@ from selenium.webdriver.support import expected_conditions as EC
 TARGET_WINDOWS = {
     7:  "7am",
     9:  "9am",
+    10: "10am",
+    11: "11am",
     12: "12pm",
+    13: "1pm",
+    14: "2pm",
     15: "3pm",
+    16: "4pm",
+    17: "5pm",
     18: "6pm",
+    19: "7pm",
+    20: "8pm",
     21: "9pm",
+    22: "10pm",
     23: "11pm",
 }
 
